c4066c04eec72d59553865cc2ecd1d777a941b75
zeinu-dev <zeinusalah@gmail.com>
2025-12-10 16:34:59 +0300

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,4 +7,16 @@ export default defineConfig({
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:8000',
+        changeOrigin: true,
+      },
+      '/media': {
+        target: 'http://127.0.0.1:8000',
+        changeOrigin: true,
+      },
+    },
+  },
 });
